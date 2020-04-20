593818538fa80590fd7d65fc26f8aa1502e33b45
Ye Zhang <mr.yezhang@gmail.com>
2020-04-19 22:01:50 -0700

    Fix esptool.py parameter error for ESP32, which caused rshell can't connect to board.

--- reflashESP32.sh
+++ reflashESP32.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# curl -o .esp32firmware.bin https://micropython.org/resources/firmware/esp32-idf3-20200420-v1.12-387-g1b1ceb67b.bin
-curl -o .esp32firmware.bin https://micropython.org/resources/firmware/esp32-idf3-20191220-v1.12.bin
-esptool.py --port /dev/ttyUSB0 erase_flash && esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 .esp32firmware.bin && rshell -p /dev/ttyUSB0 --buffer-size=30
+curl -o .esp32firmware.bin https://micropython.org/resources/firmware/esp32-idf3-20200420-v1.12-387-g1b1ceb67b.bin
+# curl -o .esp32firmware.bin https://micropython.org/resources/firmware/esp32-idf3-20191220-v1.12.bin
+esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash && esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 .esp32firmware.bin && rshell -p /dev/ttyUSB0 --buffer-size=30
